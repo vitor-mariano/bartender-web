f1d4b3c98a503b98169ecf295e545c94ab3914b6
Vitor Mariano <vmatheus.mariano@gmail.com>
2022-05-23 13:32:42 -0300

    build: setup MUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
+import { CssBaseline } from "@mui/material";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
