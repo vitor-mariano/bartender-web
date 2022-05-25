49ee0d6b8ae1db7eda872b4e6cf51001b140e78b
Vitor Mariano <vmatheus.mariano@gmail.com>
2022-05-25 14:02:08 -0300

    feat(order): request order

--- src/app/order/models.ts
+++ src/app/order/models.ts
@@ -1,5 +1,4 @@
 export interface OrderItem {
-  id: string;
   description: string;
   price: number;
   quantity: number;
--- src/app/order/server.tsx
+++ src/app/order/server.tsx
@@ -1,27 +1,38 @@
 import { GetServerSideProps } from "next";
+import { getAxiosInstance } from "src/app/shared/providers/api";
 import { OrderItem } from "./models";
 
 export interface Props {
   items: OrderItem[];
 }
 
-const getServerSideProps: GetServerSideProps<Props> = async () => {
-  // FIXME: request from API.
-  const items: OrderItem[] = [
-    {
-      id: "1",
-      description: "Still water 500ml",
-      price: 2.5,
-      quantity: 1,
-      delivered: false,
-    },
-  ];
-
-  return {
-    props: {
-      items,
-    },
+interface ApiResponse {
+  order: {
+    orderId: string;
+    items: OrderItem[];
   };
+}
+
+const getServerSideProps: GetServerSideProps<Props> = async ({ query }) => {
+  const http = getAxiosInstance();
+  const { orderId } = query;
+
+  try {
+    const result = await http.get<ApiResponse>("/orders/" + orderId);
+
+    const { items } = result.data.order;
+
+    return {
+      props: { items },
+    };
+  } catch (error: any) {
+    if (error.response?.status === 404)
+      return {
+        notFound: true,
+      };
+
+    throw error;
+  }
 };
 
 export default getServerSideProps;
