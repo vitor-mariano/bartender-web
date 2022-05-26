3611fc6d06490600ab89f5f75397855c4f7f96ad
Vitor Mariano <vmatheus.mariano@gmail.com>
2022-05-26 12:43:14 -0300

    feat: allow API client to be authenticated

--- src/app/shared/providers/api.ts
+++ src/app/shared/providers/api.ts
@@ -1,11 +1,16 @@
 import axios from "axios";
 import { getConfig } from "src/app/shared/config";
 
-export function getAxiosInstance() {
+export function getAxiosInstance(accessToken?: string) {
   const config = getConfig();
 
   const axiosInstance = axios.create({
     baseURL: config.api.url,
+    ...(accessToken && {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }),
   });
 
   return axiosInstance;
