89c321c43d5cf005ee44508b5d3a36594d1e136c
Vitor Mariano <vmatheus.mariano@gmail.com>
2022-05-27 21:56:59 -0300

    build: install toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import type { AppProps } from "next/app";
 import { CssBaseline } from "@mui/material";
 import { UserProvider } from "@auth0/nextjs-auth0";
@@ -7,6 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <UserProvider>
       <CssBaseline />
       <Component {...pageProps} />
+      <ToastContainer />
     </UserProvider>
   );
 }
